bf6b3706bd6353acc717e81e16b71893dd7765f2
yangqian <1289089651@qq.com>
2018-08-13 21:02:50 +0800

    bbs项目的第二个版本

--- blog01/views.py
+++ blog01/views.py
@@ -128,18 +128,225 @@ class Login(views.View):
             res["msg"] = '用户名密码不正确'
         return JsonResponse(res)
 
-################ 登录验证码 #######################
+################ 登录验证码 版本1 #######################
+# def v_code(request):
+# # 随机生成图片
+#     from PIL import Image
+#     import random
+#     # 生成随机颜色的方法
+#     def random_color():
+#         return random.randint(0,255),random.randint(0,255),random.randint(0,255)
+#     # 生成图片对象
+#     image_obj = Image.new(
+#         'RGB',  # 生成图片的模式
+#         (250,35), # 生成图片的大小
+#         random_color()  # 随机生成图片的颜色
+#     )
+# # 直接将生成的图片保存在内存中
+#     from io import BytesIO
+#     f = BytesIO()
+#     image_obj.save(f,'png')
+#     # 从内存中读取图片数据
+#     data = f.getvalue()
+#     return HttpResponse(data,content_type='image/png')
+
+################ 登录验证码 版本2 #######################
+# def v_code(request):
+# # 随机生成图片
+#     from PIL import Image
+#     import random
+#     # 生成随机颜色的方法
+#     def random_color():
+#         return random.randint(0,255),random.randint(0,255),random.randint(0,255)
+#     # 生成图片对象
+#     image_obj = Image.new(
+#         'RGB',  # 生成图片的模式
+#         (250,35), # 生成图片的大小
+#         random_color()  # 随机生成图片的颜色
+#     )
+#
+# # 生成一个准备写字的画笔
+#     from PIL import ImageDraw , ImageFont
+#     draw_obj = ImageDraw.Draw(image_obj)    # 在哪里写
+#     font_obj = ImageFont.truetype('static/font/kumo.ttf',size=28)   # 加载本地的字体文件
+#
+# # 生成随机验证码
+#     tmp = []
+#     for i in range(5):
+#         n = str(random.randint(0,9))
+#         l = chr(random.randint(65,90))
+#         u = chr(random.randint(97,122))
+#         r = random.choice([n,l,u])
+#         tmp.append(r)
+#         # 每一次取到要写的东西之后，往图片上写
+#         draw_obj.text(
+#             (i * 45 + 25 , 0), # 坐标---->（x,y）
+#             r,  # 内容
+#             fill=random_color(), # 颜色
+#             font=font_obj # 字体
+#         )
+#
+# # 得到最终的验证码
+#     v_code = ''.join(tmp)
+#
+# # 由于设置为全局变量，多个浏览器没办法区别验证码，故保存为全局变量是万万不可的！
+# # 将盖茨请求生成的验证码保存在该请求对应的session数据中
+#     request.session["v_code"] = v_code.upper()
+#
+#
+# # 直接将生成的图片保存在内存中
+#     from io import BytesIO
+#     f = BytesIO()
+#     image_obj.save(f,'png')
+#     # 从内存中读取图片数据
+#     data = f.getvalue()
+#     return HttpResponse(data,content_type='image/png')
+
+
+################ 登录验证码 版本3 #######################
+# def v_code(request):
+# # 随机生成图片
+#     from PIL import Image
+#     import random
+#     # 生成随机颜色的方法
+#     def random_color():
+#         return random.randint(0,255),random.randint(0,255),random.randint(0,255)
+#     # 生成图片对象
+#     image_obj = Image.new(
+#         'RGB',  # 生成图片的模式
+#         (250,35), # 生成图片的大小
+#         random_color()  # 随机生成图片的颜色
+#     )
+#
+# # 生成一个准备写字的画笔
+#     from PIL import ImageDraw , ImageFont
+#     draw_obj = ImageDraw.Draw(image_obj)    # 在哪里写
+#     font_obj = ImageFont.truetype('static/font/kumo.ttf',size=28)   # 加载本地的字体文件
+#
+# # 生成随机验证码
+#     tmp = []
+#     for i in range(5):
+#         n = str(random.randint(0,9))
+#         l = chr(random.randint(65,90))
+#         u = chr(random.randint(97,122))
+#         r = random.choice([n,l,u])
+#         tmp.append(r)
+#         # 每一次取到要写的东西之后，往图片上写
+#         draw_obj.text(
+#             (i * 45 + 25 , 0), # 坐标---->（x,y）
+#             r,  # 内容
+#             fill=random_color(), # 颜色
+#             font=font_obj # 字体
+#         )
+#
+# # 加干扰线
+#     width = 250 # 图片宽度(防止越界)
+#     height = 35 # 图片高度(防止越界)
+#     for i in range(5):
+#         x1 = random.randint(0,width)
+#         x2 = random.randint(0,width)
+#         y1 = random.randint(0,height)
+#         y2 = random.randint(0,height)
+#         draw_obj.line(
+#             (x1,y1,x2,y2),
+#             fill=random_color(), # 随机生成颜色
+#         )
+#
+# # 得到最终的验证码
+#     v_code = ''.join(tmp)
+#
+# # 由于设置为全局变量，多个浏览器没办法区别验证码，故保存为全局变量是万万不可的！
+# # 将盖茨请求生成的验证码保存在该请求对应的session数据中
+#     request.session["v_code"] = v_code.upper()
+#
+#
+# # 直接将生成的图片保存在内存中
+#     from io import BytesIO
+#     f = BytesIO()
+#     image_obj.save(f,'png')
+#     # 从内存中读取图片数据
+#     data = f.getvalue()
+#     return HttpResponse(data,content_type='image/png')
+
+
+################ 登录验证码 版本4 #######################
 def v_code(request):
-# 随机生成图片
+    # 随机生成图片
     from PIL import Image
     import random
     # 生成随机颜色的方法
     def random_color():
-        return random.randint(0,255),random.randint(0,255),random.randint(0,255)
+        return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+
     # 生成图片对象
     image_obj = Image.new(
         'RGB',  # 生成图片的模式
-        (250,35), # 生成图片的大小
+        (250, 35),  # 生成图片的大小
         random_color()  # 随机生成图片的颜色
     )
-# 得到最终的验证码
+
+    # 生成一个准备写字的画笔
+    from PIL import ImageDraw, ImageFont
+    draw_obj = ImageDraw.Draw(image_obj)  # 在哪里写
+    font_obj = ImageFont.truetype('static/font/kumo.ttf', size=28)  # 加载本地的字体文件
+
+    # 生成随机验证码
+    tmp = []
+    for i in range(5):
+        n = str(random.randint(0, 9))
+        l = chr(random.randint(65, 90))
+        u = chr(random.randint(97, 122))
+        r = random.choice([n, l, u])
+        tmp.append(r)
+        # 每一次取到要写的东西之后，往图片上写
+        draw_obj.text(
+            (i * 45 + 25, 0),  # 坐标---->（x,y）
+            r,  # 内容
+            fill=random_color(),  # 颜色
+            font=font_obj  # 字体
+        )
+
+    # 加干扰线
+    width = 250  # 图片宽度(防止越界)
+    height = 35  # 图片高度(防止越界)
+    for i in range(5):
+        x1 = random.randint(0, width)
+        x2 = random.randint(0, width)
+        y1 = random.randint(0, height)
+        y2 = random.randint(0, height)
+        draw_obj.line(
+            (x1, y1, x2, y2),
+            fill=random_color(),  # 随机生成颜色
+        )
+
+    # 加干扰点
+    width = 250  # 图片宽度(防止越界)
+    height = 35  # 图片高度(防止越界)
+    for i in range(5):
+        draw_obj.point(
+            [random.randint(0,width),random.randint(0,height)],
+            fill=random_color(),
+        )
+        x = random.randint(0,width)
+        y = random.randint(0,height)
+        draw_obj.arc(
+            (x,y,x+4,y+4),
+            0,
+            90,
+            fill=random_color()
+        )
+
+    # 得到最终的验证码
+    v_code = ''.join(tmp)
+
+    # 由于设置为全局变量，多个浏览器没办法区别验证码，故保存为全局变量是万万不可的！
+    # 将盖茨请求生成的验证码保存在该请求对应的session数据中
+    request.session["v_code"] = v_code.upper()
+
+    # 直接将生成的图片保存在内存中
+    from io import BytesIO
+    f = BytesIO()
+    image_obj.save(f, 'png')
+    # 从内存中读取图片数据
+    data = f.getvalue()
+    return HttpResponse(data, content_type='image/png')
